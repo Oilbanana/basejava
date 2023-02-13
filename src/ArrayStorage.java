62e2dd6223c0b46af6e0c9a0ddfea38a9273673d
Anastas Khionidi <98845326+Oilbanana@users.noreply.github.com>
2023-02-13 11:02:24 +0300

    Добавил реализацию методов getAll, size в ArrayStorage (Все вышеописанные в этом коммите методы не протестированы) Методы get, save - работают исправно - протестировал MainTestArrayStorage

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,45 +1,42 @@
 /**
  * Array based storage for Resumes
  */
+
 import java.util.Arrays;
+
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
 
     void clear() {
-        Arrays.fill(storage,null);
+        Arrays.fill(storage, null);
     }
 
     void save(Resume r) {
-        storage[getIndexWithoutNull()]= r;
+        storage[getIndexWithoutNull()] = r;
     }
 
     Resume get(String uuid) {
-        for (Resume resume: storage){
-            if(resume.uuid.equals(uuid)){
-                return resume;
-            }
-            else {
-                System.out.println("Резюме с таким id нет");
+        for (int i = 0; i < getIndexWithoutNull(); i++) {
+            if (storage[i].uuid.equals(uuid)) {
+                return storage[i];
             }
         }
+        System.out.println("Резюме с таким id нет");
         return null;
     }
 
     void delete(String uuid) {
-        int bufferForIndex=0;//Переменная для хранение индекса удаленного резюме
-        int bufferForLastIndex= getIndexWithoutNull();
-        for (int i = 0; i < bufferForLastIndex ; i++) {
-            if (storage[i].uuid.equals(uuid)){
+        int bufferForIndex = 0;//Переменная для хранение индекса удаленного резюме
+        int bufferForLastIndex = getIndexWithoutNull();
+        for (int i = 0; i < bufferForLastIndex; i++) {
+            if (storage[i].uuid.equals(uuid)) {
                 bufferForIndex = i;
                 storage[i] = null;
             }
-            else{
-                System.out.println("Резюме с таким id нет");
-            }
+        }
 
-            for (int j = bufferForIndex+1; j < bufferForLastIndex ; j++) {
-                storage[j-1] = storage[j];
-            }
+        for (int j = bufferForIndex + 1; j < bufferForLastIndex; j++) {
+            storage[j - 1] = storage[j];
         }
     }
 
@@ -47,17 +44,17 @@ public class ArrayStorage {
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        return Arrays.copyOf(storage,getIndexWithoutNull());
     }
 
     int size() {
-        return 0;
+        return getIndexWithoutNull();
     }
 
     //Метод для опредления индекса без null в массиве Storage
-    int getIndexWithoutNull(){
+    int getIndexWithoutNull() {
         int i = 0;
-        while (storage[i] != null){
+        while (storage[i] != null) {
             i++;
         }
         return i;
