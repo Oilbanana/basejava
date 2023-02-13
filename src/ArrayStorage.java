0f41ea1c4bb1f9c51c9ede81f467e1f3d0e76f8f
Anastas Khionidi <98845326+Oilbanana@users.noreply.github.com>
2023-02-13 10:09:15 +0300

    Добавил реализацию метода delete в ArrayStorage (Все вышеописанные методы не протестированы)

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -19,17 +19,26 @@ public class ArrayStorage {
                 return resume;
             }
             else {
-                System.out.println("Резюме с  таким id нет");
+                System.out.println("Резюме с таким id нет");
             }
         }
         return null;
     }
 
     void delete(String uuid) {
-        int bufferForIndex;
-        for (int i = 0; i < getIndexWithoutNull()-1 ; i++) {
+        int bufferForIndex=0;//Переменная для хранение индекса удаленного резюме
+        int bufferForLastIndex= getIndexWithoutNull();
+        for (int i = 0; i < bufferForLastIndex ; i++) {
             if (storage[i].uuid.equals(uuid)){
+                bufferForIndex = i;
+                storage[i] = null;
+            }
+            else{
+                System.out.println("Резюме с таким id нет");
+            }
 
+            for (int j = bufferForIndex+1; j < bufferForLastIndex ; j++) {
+                storage[j-1] = storage[j];
             }
         }
     }
@@ -45,6 +54,7 @@ public class ArrayStorage {
         return 0;
     }
 
+    //Метод для опредления индекса без null в массиве Storage
     int getIndexWithoutNull(){
         int i = 0;
         while (storage[i] != null){
