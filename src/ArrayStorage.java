96099e460bb48a67639ecee603d2b8e218298c31
Anastas Khionidi <98845326+Oilbanana@users.noreply.github.com>
2023-02-13 09:28:16 +0300

    Добавил реализацию метода save,get в ArrayStorage

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,20 +1,37 @@
 /**
  * Array based storage for Resumes
  */
+import java.util.Arrays;
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
 
     void clear() {
+        Arrays.fill(storage,null);
     }
 
     void save(Resume r) {
+        storage[getIndexWithoutNull()]= r;
     }
 
     Resume get(String uuid) {
+        for (Resume resume: storage){
+            if(resume.uuid.equals(uuid)){
+                return resume;
+            }
+            else {
+                System.out.println("Резюме с  таким id нет");
+            }
+        }
         return null;
     }
 
     void delete(String uuid) {
+        int bufferForIndex;
+        for (int i = 0; i < getIndexWithoutNull()-1 ; i++) {
+            if (storage[i].uuid.equals(uuid)){
+
+            }
+        }
     }
 
     /**
@@ -27,4 +44,13 @@ public class ArrayStorage {
     int size() {
         return 0;
     }
+
+    int getIndexWithoutNull(){
+        int i = 0;
+        while (storage[i] != null){
+            i++;
+        }
+        return i;
+    }
+
 }
