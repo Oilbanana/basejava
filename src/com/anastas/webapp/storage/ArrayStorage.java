4ce0e1e8db3757416dfac68d2bf7b65fff00d82e
Anastas Khionidi <98845326+Oilbanana@users.noreply.github.com>
2023-02-20 10:23:17 +0300

    Добавил пакеты

--- src/com/anastas/webapp/storage/ArrayStorage.java
+++ src/com/anastas/webapp/storage/ArrayStorage.java
@@ -1,6 +1,9 @@
-/**
+package com.anastas.webapp.storage;
+/*
  * Array based storage for Resumes
  */
+
+import com.anastas.webapp.model.Resume;
 
 import java.util.Arrays;
 
@@ -8,28 +11,28 @@ public class ArrayStorage {
     Resume[] storage = new Resume[10000];
     private int size = 0;
 
-    void clear() {
+    public void clear() {
         Arrays.fill(storage, 0, size, null);
         size = 0;
     }
 
-    void save(Resume r) {
+    public void save(Resume r) {
         storage[size] = r;
-        size ++;
+        size++;
     }
 
-    Resume get(String uuid) {
+    public Resume get(String uuid) {
         for (int i = 0; i < size; i++) {
-            if (storage[i].uuid.equals(uuid)) {
+            if (storage[i].getUuid().equals(uuid)) {
                 return storage[i];
             }
         }
         return null;
     }
 
-    void delete(String uuid) {
+    public void delete(String uuid) {
         for (int i = 0; i < size; i++) {
-            if (storage[i].uuid.equals(uuid)) {
+            if (storage[i].getUuid().equals(uuid)) {
                 storage[i] = storage[size - 1];
                 size--;
                 storage[size] = null;
@@ -41,11 +44,11 @@ public class ArrayStorage {
     /**
      * @return array, contains only Resumes in storage (without null)
      */
-    Resume[] getAll() {
+    public Resume[] getAll() {
         return Arrays.copyOf(storage, size);
     }
 
-    int size() {
+    public int size() {
         return size;
     }
 
