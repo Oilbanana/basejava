1d3c6263e697ad026670b78e8b9feb8c6bd78353
Anastas Khionidi <98845326+Oilbanana@users.noreply.github.com>
2023-02-27 02:08:01 +0300

    Шаблонные и общие  методы перенесены в AbstractArrayStorage

--- src/com/anastas/webapp/storage/AbstractArrayStorage.java
+++ src/com/anastas/webapp/storage/AbstractArrayStorage.java
@@ -2,25 +2,69 @@ package com.anastas.webapp.storage;
 
 import com.anastas.webapp.model.Resume;
 
+import java.util.Arrays;
+
 public abstract class AbstractArrayStorage implements Storage {
     public static final int STORAGE_LIMIT = 10000;
     protected final Resume[] storage = new Resume[STORAGE_LIMIT];
     protected int size = 0;
 
-    public Resume get(String uuid) {
-        if (getIndex(uuid) != -1) {
-            for (int i = 0; i < size; i++) {
-                if (storage[i].getUuid().equals(uuid))
-                    return storage[i];
-            }
-        } else {
-            System.out.println("ERROR: Введите существующее резюме,а не " + uuid);
-        }
-        return null;
+    // Общие методы
+    public void clear() {
+        Arrays.fill(storage, 0, size, null);
+        size = 0;
+    }
+
+    public Resume[] getAll() {
+        return Arrays.copyOf(storage, size);
     }
 
     public int size() {
         return size;
+    }
+
+    // Шаблонные методы ниже, использующие getIndex
+    public Resume get(String uuid) {
+        int index = getIndex(uuid);
+        if (index != -1) {
+            System.out.println("ERROR: Введите существующее резюме,а не " + uuid);
+            return null;
+        }
+        return storage[index];
+    }
+
+    public void update(Resume r) {
+        int index = getIndex(r.getUuid());
+        if (index != -1) {
+            storage[index] = r;
+        } else {
+            System.out.println("ERROR: Введите существующее резюме, а не " + r.getUuid());
+        }
+
+    }
+
+    public void save(Resume r) {
+        int index = getIndex(r.getUuid());
+        if (size > STORAGE_LIMIT) {
+            System.out.println("ERROR: Хранилище переполнено.");
+        }
+        if (index == -1) {
+            storage[size] = r;
+            size++;
+        } else {
+            System.out.println("ERROR: Введите новое резюме, а не существующее: " + r.getUuid());
+        }
+    }
+
+    public void delete(String uuid) {
+        int index = getIndex(uuid);
+        if (index != -1) {
+            storage[index] = storage[size - 1];
+            size--;
+            storage[size] = null;
+        } else {
+            System.out.println("ERROR: Введите существующее резюме, а не " + uuid);
+        }
     }
 
     protected abstract int getIndex(String uuid);
--- src/com/anastas/webapp/storage/ArrayStorage.java
+++ src/com/anastas/webapp/storage/ArrayStorage.java
@@ -3,54 +3,8 @@ package com.anastas.webapp.storage;
  * Array based storage for Resumes
  */
 
-import com.anastas.webapp.model.Resume;
-
-import java.util.Arrays;
-
 public class ArrayStorage extends AbstractArrayStorage {
-    public void clear() {
-        Arrays.fill(storage, 0, size, null);
-        size = 0;
-    }
-
-    public void update(Resume r) {
-        int index = getIndex(r.getUuid());
-        if (index != -1) {
-            storage[index] = r;
-        } else {
-            System.out.println("ERROR: Введите существующее резюме, а не " + r.getUuid());
-        }
-
-    }
-
-    public void save(Resume r) {
-        int index = getIndex(r.getUuid());
-        if (size > STORAGE_LIMIT) {
-            System.out.println("ERROR: Хранилище переполнено.");
-        }
-        if (index == -1) {
-            storage[size] = r;
-            size++;
-        } else {
-            System.out.println("ERROR: Введите новое резюме, а не существующее: " + r.getUuid());
-        }
-    }
-
-    public void delete(String uuid) {
-        int index = getIndex(uuid);
-        if (index != -1) {
-            storage[index] = storage[size - 1];
-            size--;
-            storage[size] = null;
-        } else {
-            System.out.println("ERROR: Введите существующее резюме, а не " + uuid);
-        }
-    }
-
-    public Resume[] getAll() {
-        return Arrays.copyOf(storage, size);
-    }
-
+    @Override
     protected int getIndex(String uuid) {
         for (int i = 0; i <= size - 1; i++) {
             if (storage[i].getUuid().equals(uuid)) {
