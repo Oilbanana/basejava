4d90bdd84eed83ea1e08fbe66ab64b85ca719c7a
Anastas Khionidi <98845326+Oilbanana@users.noreply.github.com>
2023-03-05 20:59:03 +0300

    Добавлены классы для тестирования JUnit

---
+++ test/com/anastas/webapp/storage/AbstractArrayStorageTest.java
@@ -0,0 +1,36 @@
+package com.anastas.webapp.storage;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AbstractArrayStorageTest {
+
+    @Test
+    void clear() {
+    }
+
+    @Test
+    void getAll() {
+    }
+
+    @Test
+    void size() {
+    }
+
+    @Test
+    void get() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void save() {
+    }
+
+    @Test
+    void delete() {
+    }
+}
---
+++ test/com/anastas/webapp/storage/SortedArrayStorageTest.java
@@ -0,0 +1,20 @@
+package com.anastas.webapp.storage;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SortedArrayStorageTest {
+
+    @Test
+    void getIndex() {
+    }
+
+    @Test
+    void insertResume() {
+    }
+
+    @Test
+    void deleteResume() {
+    }
+}
