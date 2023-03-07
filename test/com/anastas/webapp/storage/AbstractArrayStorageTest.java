365e6d0c6ab68050792419abf412d5806cd5ca3f
Anastas Khionidi <98845326+Oilbanana@users.noreply.github.com>
2023-03-07 22:57:42 +0300

    Добавлены общие методы для уменьшения поворений- assertGet и assertSize

--- test/com/anastas/webapp/storage/AbstractArrayStorageTest.java
+++ test/com/anastas/webapp/storage/AbstractArrayStorageTest.java
@@ -10,9 +10,11 @@ public abstract class AbstractArrayStorageTest {
     private static final String UUID_1 = "uuid1";
     private static final String UUID_2 = "uuid2";
     private static final String UUID_3 = "uuid3";
+    private static final String UUID_4 = "uuid4";
     private static final Resume RESUME_1 = new Resume(UUID_1);
     private static final Resume RESUME_2 = new Resume(UUID_2);
     private static final Resume RESUME_3 = new Resume(UUID_3);
+    private static final Resume RESUME_4 = new Resume(UUID_4);
 
     AbstractArrayStorageTest(Storage storage) {
         this.storage = storage;
@@ -24,13 +26,14 @@ public abstract class AbstractArrayStorageTest {
         storage.save(RESUME_1);
         storage.save(RESUME_2);
         storage.save(RESUME_3);
-
     }
 
     @Test
     void clear() {
         storage.clear();
-        Assertions.assertEquals(0, storage.size());
+        assertSize(0);
+        Resume[] exceptedArray ={};
+        Assertions.assertArrayEquals(exceptedArray, storage.getAll());
     }
 
     @Test
@@ -41,14 +44,14 @@ public abstract class AbstractArrayStorageTest {
 
     @Test
     void size() {
-        Assertions.assertEquals(3, storage.size());
+        assertSize(3);
     }
 
     @Test
     void get() {
-        Assertions.assertEquals(RESUME_1, storage.get("uuid1"));
-        Assertions.assertEquals(RESUME_2, storage.get("uuid2"));
-        Assertions.assertEquals(RESUME_3, storage.get("uuid3"));
+        assertGet(RESUME_1);
+        assertGet(RESUME_2);
+        assertGet(RESUME_3);
     }
 
     @Test
@@ -61,5 +64,12 @@ public abstract class AbstractArrayStorageTest {
 
     @Test
     void delete() {
+    }
+
+    void assertSize(int expected){
+        Assertions.assertEquals(expected, storage.size());
+    }
+    void assertGet(Resume r){
+        Assertions.assertEquals(r, storage.get(r.getUuid()));
     }
 }
